8f275739293b98e7b36ce776274cd7ad2a691ade
Ivaylo Strandjev <istrandjev@gmail.com>
2024-12-01 14:17:42 +0200

    Remove unnecessary code

--- 1.cpp
+++ 1.cpp
@@ -1,30 +1,11 @@
-#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream> 
-#include <string>
 #include <vector>
-#include <sstream>
-#include <queue>
 #include <algorithm>
-#include <iomanip>
 #include <map>
-#include <set>
-#include <math.h>
-#include <stack>
-#include <deque>
-#include <numeric>
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
-#include <bitset>
-#include <functional>
-#include <unordered_set>
-#include <unordered_map>
-#include <regex>
 
 #define all(v) v.begin(),v.end()
 using namespace std;
 typedef long long ll;
-typedef long double ld;
 
 int main() {
     vector<ll> a, b;
